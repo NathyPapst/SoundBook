94c3559ca85d71c1b0e3aaab1303cc748f6dbdf1
ortizfrancielly <ortizfrancielly@gmail.com>
2021-09-20 17:51:27 -0300

    Adicionar label
    Adição de constraints e configurações visuais para a label referente ao título do decibelímetro

--- SoundBook/View Controllers/HomeScreenViewController.swift
+++ SoundBook/View Controllers/HomeScreenViewController.swift
@@ -11,6 +11,7 @@ class HomeScreenViewController: UIViewController {
 
     var buttonEdit: UIBarButtonItem!
     var buttonAdd: UIBarButtonItem!
+    var soundIntensityLabel: UILabel = UILabel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +23,13 @@ class HomeScreenViewController: UIViewController {
         buttonAdd = UIBarButtonItem(image: UIImage(systemName: "plus"), style: UIBarButtonItem.Style.plain, target: self, action: #selector(addObject))
         navigationItem.rightBarButtonItem = buttonAdd!
         
+        soundIntensityLabel.text = "Intensidade do Som"
+        soundIntensityLabel.textColor = UIColor(named: "textColor")
+        soundIntensityLabel.font = .systemFont(ofSize: 28, weight: .semibold)
+        
+        view.addSubview(soundIntensityLabel)
+        addConstraints()
+        
         // Do any additional setup after loading the view.
     }
 
@@ -30,6 +38,16 @@ class HomeScreenViewController: UIViewController {
     }
     
     @objc func addObject(){
+        
+    }
+    
+    func addConstraints(){
+        
+        //constraints do sound intensity
+        soundIntensityLabel.translatesAutoresizingMaskIntoConstraints = false
+        soundIntensityLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: view.frame.height/7).isActive = true
+        soundIntensityLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 34).isActive = true
+        soundIntensityLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -34).isActive = true
         
     }
 
