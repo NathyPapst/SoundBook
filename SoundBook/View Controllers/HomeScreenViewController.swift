d89554c35e6a9db8749e8c68b5239d343153272f
ortizfrancielly <ortizfrancielly@gmail.com>
2021-09-21 14:47:58 -0300

    Adicionar labels do decibelímetro

--- SoundBook/View Controllers/HomeScreenViewController.swift
+++ SoundBook/View Controllers/HomeScreenViewController.swift
@@ -14,6 +14,10 @@ class HomeScreenViewController: UIViewController {
     var soundIntensityLabel: UILabel = UILabel()
     var decibelSpace: UIView = UIView()
     var indicatorLabel: UILabel = UILabel()
+    var highLabel: UILabel = UILabel()
+    var moderateLabel: UILabel = UILabel()
+    var lowLabel: UILabel = UILabel()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,9 +39,26 @@ class HomeScreenViewController: UIViewController {
         indicatorLabel.textColor = UIColor(named: "textColor")
         indicatorLabel.font = .systemFont(ofSize: (view.frame.height * 0.028), weight: .regular)
         
+        highLabel.text = "Alto"
+        highLabel.textColor = UIColor(red: 237.0/255.0, green: 85.0/255.0, blue: 75.0/255.0, alpha: 1.0)
+        highLabel.font = .systemFont(ofSize: (18), weight: .semibold)
+        
+        
+        moderateLabel.text = "Médio"
+        moderateLabel.textColor = UIColor(red: 244.0/255.0, green: 210.0/255.0, blue: 80.0/255.0, alpha: 1.0)
+        moderateLabel.font = .systemFont(ofSize: (18), weight: .semibold)
+        
+        lowLabel.text = "Baixo"
+        lowLabel.textColor = UIColor(red: 51.0/255.0, green: 219.0/255.0, blue: 161.0/255.0, alpha: 1.0)
+        lowLabel.font = .systemFont(ofSize: (18), weight: .semibold)
+        
         view.addSubview(soundIntensityLabel)
         view.addSubview(decibelSpace)
         view.addSubview(indicatorLabel)
+        view.addSubview(highLabel)
+        view.addSubview(moderateLabel)
+        view.addSubview(lowLabel)
+        
         
         addConstraints()
         
@@ -72,6 +93,21 @@ class HomeScreenViewController: UIViewController {
         indicatorLabel.leadingAnchor.constraint(equalTo: decibelSpace.trailingAnchor, constant: 10).isActive = true
         indicatorLabel.topAnchor.constraint(equalTo: soundIntensityLabel.bottomAnchor, constant: view.frame.height/30).isActive = true
         indicatorLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20).isActive = true
+        
+        //constraints lowLabel
+        lowLabel.translatesAutoresizingMaskIntoConstraints = false
+        lowLabel.topAnchor.constraint(equalTo: decibelSpace.bottomAnchor).isActive = true
+        lowLabel.centerXAnchor.constraint(equalTo: decibelSpace.centerXAnchor, constant: -80).isActive = true
+        
+        //constraints moderateLabel
+        moderateLabel.translatesAutoresizingMaskIntoConstraints = false
+        moderateLabel.topAnchor.constraint(equalTo: decibelSpace.bottomAnchor).isActive = true
+        moderateLabel.centerXAnchor.constraint(equalTo: decibelSpace.centerXAnchor).isActive = true
+        
+        //constraints highLabel
+        highLabel.translatesAutoresizingMaskIntoConstraints = false
+        highLabel.topAnchor.constraint(equalTo: decibelSpace.bottomAnchor).isActive = true
+        highLabel.centerXAnchor.constraint(equalTo: decibelSpace.centerXAnchor, constant: 80).isActive = true
     }
 
 }
