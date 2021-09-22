ddf5ab2babf3d865c0d53a944097107acce042d4
ortizfrancielly <ortizfrancielly@gmail.com>
2021-09-22 17:59:31 -0300

    Inserir características de célula da table view - parte 2
    imagem, título, descrição de som e relógio

--- SoundBook/Cell Style /CellStyle.swift
+++ SoundBook/Cell Style /CellStyle.swift
@@ -18,13 +18,14 @@ class CellStyle: UITableViewCell{
         let title = UILabel()
         title.textColor = UIColor(named: "textColor")
         title.font = .systemFont(ofSize: (18), weight: .semibold)
-        title.backgroundColor = .black
+        title.text = "Liquidificador"
         return title
     }()
-    var infoLabel: UILabel = {
+    var infoSoundLabel: UILabel = {
         let info = UILabel()
         info.textColor = UIColor(named: "textColor")
         info.font = .systemFont(ofSize: (18), weight: .regular)
+        info.text = "120 dB | Médio"
         return info
     }()
     var imageSymbol: UIImageView = {
@@ -36,16 +37,18 @@ class CellStyle: UITableViewCell{
     var timeLabel: UILabel = {
         let time = UILabel()
         time.textColor = UIColor(named: "textColor")
+        time.text = "20h - 07h"
         time.font = .systemFont(ofSize: (18), weight: .regular)
         return time
     }()
+
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         imageCell.layer.cornerRadius = (self.frame.height/3.5)
         self.addSubview(imageCell)
         self.addSubview(titleLabel)
-        self.addSubview(infoLabel)
+        self.addSubview(infoSoundLabel)
         self.addSubview(imageSymbol)
         self.addSubview(timeLabel)
         addConstraints()
@@ -68,8 +71,30 @@ class CellStyle: UITableViewCell{
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 10).isActive = true
         titleLabel.leadingAnchor.constraint(equalTo: imageCell.trailingAnchor, constant: 10).isActive = true
-        titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -self.frame.width/2).isActive = true
+        titleLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -self.frame.width/5).isActive = true
         titleLabel.bottomAnchor.constraint(equalTo: self.topAnchor, constant: 30).isActive = true
+
+        //constraint infoSoundLabel
+        infoSoundLabel.translatesAutoresizingMaskIntoConstraints = false
+        infoSoundLabel.leadingAnchor.constraint(equalTo: imageCell.trailingAnchor, constant: 10).isActive = true
+        infoSoundLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -self.frame.height/7).isActive = true
+        infoSoundLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: self.frame.height/20).isActive = true
+        infoSoundLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -self.frame.width/2).isActive = true
+        
+        //constraint imageSymbol
+        imageSymbol.translatesAutoresizingMaskIntoConstraints = false
+        imageSymbol.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: self.frame.height/4).isActive = true
+        imageSymbol.leadingAnchor.constraint(equalTo: infoSoundLabel.trailingAnchor, constant: 30).isActive = true
+        imageSymbol.heightAnchor.constraint(equalToConstant: 22).isActive = true
+        imageSymbol.widthAnchor.constraint(equalToConstant: 22).isActive = true
+        
+        //constraint timeLabel
+        timeLabel.translatesAutoresizingMaskIntoConstraints = false
+        timeLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -self.frame.height/7).isActive = true
+        timeLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: self.frame.height/20).isActive = true
+        timeLabel.leadingAnchor.constraint(equalTo: imageSymbol.trailingAnchor, constant:  5).isActive = true
+        timeLabel.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        
     }
 }
 
